ff5cd35bd77075453079491e574ce82825f6469a
Daniele Sangalli <sangallidaniele@gmail.com>
2018-05-04 14:12:29 +0100

    mongo db lib upgrade and revision of tests

--- src/connection.js
+++ src/connection.js
@@ -1,38 +1,41 @@
-import { MongoClient } from "mongodb";
-import * as R from "ramda";
+import {MongoClient} from "mongodb"
+import * as R from "ramda"
 
-let connectionPool = {};
+let connectionPool = {}
 
 export function getConnection(connectionObject) {
   return new Promise((resolve, reject) => {
     const connectionString =
       typeof connectionObject === "string"
         ? connectionObject
-        : connectionObject.connectionString;
+        : connectionObject.connectionString
     const options =
       typeof connectionObject === "string"
         ? {}
-        : R.omit(["connectionString"], connectionObject);
-    let hasConnection = R.pick([connectionString], connectionPool);
+        : R.omit(["connectionString"], connectionObject)
+    let hasConnection = R.pick([connectionString], connectionPool)
     if (R.keys(hasConnection).length !== 0) {
-      resolve(connectionPool[connectionString]);
+      resolve(connectionPool[connectionString])
     } else {
-      MongoClient.connect(connectionString, options, (err, db) => {
-        if (err) {
-          reject(err);
+      MongoClient.connect(connectionString, options, (err, client) => {
+        let database = connectionString.match(/.+:\d+\/([a-z-_.]+)/i)[1]
+
+        if (err || database === "") {
+          reject(err)
         } else {
-          connectionPool[connectionString] = db;
-          resolve(db);
+          let db = client.db(database)
+          connectionPool[connectionString] = db
+          resolve(db)
         }
-      });
+      })
     }
-  });
+  })
 }
 
 let getCollection = R.curry((collectionName, db) => {
-  return db.collection(collectionName);
-});
+  return db.collection(collectionName)
+})
 
 export let connectToCollection = collectionName => {
-  return R.composeP(getCollection(collectionName), getConnection);
-};
+  return R.composeP(getCollection(collectionName), getConnection)
+}
--- src/helpers.js
+++ src/helpers.js
@@ -1,72 +1,72 @@
-import * as R from "ramda";
-import { connectToCollection, getConnection } from "./connection";
+import * as R from "ramda"
+import {connectToCollection, getConnection} from "./connection"
 
 export let resultResolver = (resolve, reject) => {
   return (err, result) => {
     if (err) {
-      reject(err);
+      reject(err)
     } else {
-      resolve(result);
+      resolve(result)
     }
-  };
-};
+  }
+}
 
 export let runner = R.curry((fn, connectionString, collectionName) => {
   return () => {
-    return mongoFnRunner(fn, collectionName, connectionString);
-  };
-});
+    return mongoFnRunner(fn, collectionName, connectionString)
+  }
+})
 
 export let funcBuilder = R.curry(
   (mongoFn, input, connectionString, collectionName) => {
     return R.curryN(input.length, (...args) => {
-      let inputValue = input.apply(null, args);
+      let inputValue = input.apply(null, args)
       return mongoFnRunner(
         mongoFn(inputValue),
         collectionName,
         connectionString
-      );
-    });
+      )
+    })
   }
-);
+)
 
 export let funcBuilder2 = R.curry(
   (mongoFn, query, update, connectionString, collectionName) => {
     return R.curryN(query.length, (...args) => {
-      let queryValue = query.apply(null, args);
+      let queryValue = query.apply(null, args)
       if (typeof update === "object") {
         return mongoFnRunner(
           mongoFn(queryValue, update),
           collectionName,
           connectionString
-        );
+        )
       } else {
         return R.curryN(query.length, (...args) => {
-          let updateValue = update.apply(null, args);
+          let updateValue = update.apply(null, args)
           return mongoFnRunner(
             mongoFn(queryValue, updateValue),
             collectionName,
             connectionString
-          );
-        });
+          )
+        })
       }
-    });
+    })
   }
-);
+)
 
 export function createInputFn(input) {
-  let inputFn = input;
+  let inputFn = input
   if (typeof input === "object" || typeof input === "string") {
     inputFn = function() {
-      return input;
-    };
+      return input
+    }
   }
-  return inputFn;
+  return inputFn
 }
 
 function mongoFnRunner(fn, collectionName, connectionString) {
-  let connector = R.composeP(fn, connectToCollection(collectionName));
-  return connector(connectionString);
+  let connector = R.composeP(fn, connectToCollection(collectionName))
+  return connector(connectionString)
 }
 
 export function stats(connectionString) {
@@ -75,5 +75,5 @@ export function stats(connectionString) {
       new Promise((resolve, reject) =>
         db.stats(resultResolver(resolve, reject))
       )
-  );
+  )
 }
